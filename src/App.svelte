efcb226f40d8aa017dc29628baa9a46bbd81d7c8
Dmitry Tishkin <dmitrytishkin@icloud.com>
2019-10-27 12:08:56 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,98 @@
+<script>
+  import { Router, Link, Route } from "svelte-routing";
+	import Incom from './routes/Incom/Incom.svelte';
+	import Placeman from './routes/Placeman/Placeman.svelte';
+	import Home from './routes/Home/Home.svelte';
+	import Maps from './routes/Maps/Maps.svelte';
+
+	export let url;
+</script>
+
+<style>
+	.app-outlet {
+		width: 100%;
+		margin: 0 auto;
+		height: 100%;
+		display: flex;
+    flex-direction: column;
+    justify-content: center;
+	}
+
+  :global(body) {
+		--mdc-theme-primary: #54B4EE;
+		--mdc-theme-secondary: #54B4EE;
+	}
+	:global(a:hover) {
+		text-decoration: none;
+	}
+	:global(.mdc-button) {
+		width: 100%;
+		text-transform: none;
+		background-color: #54B4EE;
+		font-family: 'Gilroy';
+		font-weight: bold;
+		box-shadow: none !important;
+	}
+	:global(.mdc-button.selected) {
+		background-color: #54B4EEAA;
+	}
+	:global(.mdc-select) {
+		width: 100%;
+	}
+	:global(.mdc-slider__track-container) {
+		height: 5px;
+		border-radius: 5px;
+		background-color: #C4C4C4 !important;
+	}
+	:global(.mdc-slider__thumb) {
+		transform: none;
+	}
+	:global(.mdc-select) {
+		height: 40px;
+	}
+	:global(.mdc-select__selected-text) {
+		padding: 0 12px !important;
+		height: 100%;
+		transform: none !important;
+		line-height: 40px;
+	}
+	:global(.mdc-floating-label) {
+		top: 0 !important;
+    height: 100%;
+		line-height: 40px !important;
+	}
+	:global(.mdc-select:not(.mdc-select--disabled).mdc-select--focused .mdc-floating-label) {
+		color: #54B4EE ;
+	}
+	:global(.mdc-select__dropdown-icon) {
+		top: 50%;
+    transform: translateY(-50%);
+	}
+	:global(.mdc-select--outlined.mdc-notched-outline--upgraded .mdc-floating-label--float-above, .mdc-select--outlined .mdc-notched-outline--upgraded .mdc-floating-label--float-above) {
+		transform: translateY(-35%) scale(.75);
+		display: none;
+	}
+	:global(.mdc-notched-outline__notch) {
+		width: 0px;
+		padding: 0 !important;
+		width: auto !important;
+	}
+	:global(.mdc-select--activated .mdc-select__dropdown-icon) {
+		transform: rotate(180deg) translateY(50%);
+	}
+	:global(h1), :global(h2), :global(h3), :global(h4) {
+		font-family: 'Gilroy';
+	}
+</style>
+
+<Router url="{url}">
+	<div class="app-outlet">
+		<Route path="maps/*" component="{Maps}" />
+		<div style="height: 100%; width: 400px; margin: auto;">
+			<Route path="/" component="{Home}" />
+			<Route path="incom/*" component="{Incom}" />
+			<Route path="placeman/*" component="{Placeman}" />
+		</div>
+
+	</div>
+</Router>
---
+++ src/routes/Placeman/PlacemanResult.svelte
@@ -0,0 +1,141 @@
+<script>
+  import Poll from "../../common/Poll.svelte";
+  import Button, { Label } from "@smui/button";
+  import { Link } from "svelte-routing";
+
+  let valueOutlinedEnhanced;
+  let result = Math.floor(Math.random() * 3);
+  while (document.res === result) {
+    result = Math.floor(Math.random() * 3);
+  }
+  document.res = result;
+</script>
+
+<style>
+  .done-icon {
+    background-size: auto;
+    width: 290px;
+    height: 290px;
+    margin: auto;
+    border-radius: 100%;
+    border: 12px #54b4ee solid;
+    box-sizing: border-box;
+  }
+  .done-icon0 {
+    background: url(/assets/icons/0.svg) no-repeat 50% 50%;
+  }
+  .done-icon1 {
+    background: url(/assets/icons/1.svg) no-repeat 50% 50%;
+  }
+  .done-icon2 {
+    background: url(/assets/icons/2.svg) no-repeat 50% 50%;
+  }
+  .done-title {
+    font-size: 24px;
+  }
+  .done {
+    text-align: center;
+  }
+  .info {
+    font-size: 14px;
+    color: #8b8b8b;
+    margin-bottom: 20px;
+  }
+  .social {
+    width: 100px;
+    display: flex;
+    margin: 8px auto;
+    justify-content: space-between;
+  }
+  .vk {
+    background: url(/assets/icons/vk.svg) no-repeat 50% 50%;
+    width: 28px;
+    height: 28px;
+  }
+  .twitter {
+    background: url(/assets/icons/twitter.svg) no-repeat 50% 50%;
+    width: 28px;
+    height: 28px;
+  }
+  .inst {
+    background: url(/assets/icons/insta.svg) no-repeat 50% 50%;
+    width: 28px;
+    height: 28px;
+  }
+  .share {
+    font-size: 12px;
+    color: #8b8b8b;
+  }
+  .controls {
+    display: grid;
+    grid-gap: 4px;
+  }
+</style>
+
+<div class="done">
+  {#if result === 0}
+    <div class="done-icon done-icon0" />
+    <h1 class="done-title">
+      Больше всего ты похож<br />
+      на Лаврова!
+    </h1>
+    <div class="info">
+      Это Министр иностранных дел,<br>
+      если ты вдруг не знал! Будь как он, <br>
+      поднимай экономику России!<br>
+      <br>
+      Нажимая на кнопку ниже, у тебя есть возможность<br>
+      узнать больше о Лаврове и о других,<br>
+      интересующих тебя чиновниках
+    </div>
+  {:else if result === 1}
+    <div class="done-icon done-icon1" />
+    <h1 class="done-title">
+      Твой чиновник -<br>
+      Эльвира Набиуллина
+    </h1>
+    <div class="info">
+      Она Председатель ЦБ РФ.<br>
+      Держу в курсе! Это уникальная личность!<br>
+      И ты можешь такой стать!<br>
+      <br>
+      Нажимая на кнопку ниже, у тебя есть возможность<br>
+      узнать больше о Набиуллиной и о других,<br>
+      интересующих тебя чиновниках
+    </div>
+  {:else if result === 2}
+    <div class="done-icon done-icon2" />
+    <h1 class="done-title">
+      Ты - Шойгу!<br>
+      Шучу, просто уровень<br>
+      жизни совпадает!
+    </h1>
+    <div class="info">
+      Это Министр обороны РФ!<br>
+      Держу в курсе!<br>
+      <br>
+      Нажимая на кнопку ниже, у тебя есть возможность<br>
+      узнать больше о Шойгу и о других,<br>
+      интересующих тебя чиновниках
+    </div>
+  {/if}
+  <div class="controls">
+    <a href="http://declarator.web.app">
+      <Button variant="raised">
+        <Label>Больше информации</Label>
+      </Button>
+    </a>
+
+    <Link to="/incom">
+      <Button variant="raised">
+        <Label>Пройти тест “Угадай доход”</Label>
+      </Button>
+    </Link>
+  </div>
+  <div class="social">
+    <div class="vk" />
+    <div class="twitter" />
+    <div class="inst" />
+  </div>
+  <div class="share">Расскажи друзьям!</div>
+</div>
